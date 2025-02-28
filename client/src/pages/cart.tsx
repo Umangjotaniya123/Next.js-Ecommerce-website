cc630db5ac8369e43bc02f2b1e52e548b3bad78f
Umang Jotaniya <umangjotaniya10@gmail.com>
2025-02-28 19:02:07 +0530

    Make ShippingInfo Page

--- client/src/pages/cart.tsx
+++ client/src/pages/cart.tsx
@@ -13,14 +13,12 @@ const cart = ({ data }: { data: string }) => {
 
     const [couponCode, setCouponCode] = useState<string>("");
     const [isValidCouponCode, setIsValidCouponCode] = useState<boolean>(false);
-    // const [cartItems, setCartItems] = useState<CartItem[] | []>([]);
 
     const dispatch = useDispatch();
 
     useEffect(() => {
         if (data) {
             const items: CartItem[] | [] = decryptedData(data);
-            // setCartItems(cartItems);
             items.map(item => dispatch(addToCart(item)));
         }
     }, [data])
@@ -28,12 +26,6 @@ const cart = ({ data }: { data: string }) => {
     const { cartItems, subTotal, tax, total, shippingCharges, discount } = useSelector(
         (state: { cartReducer: CartReducerInitialState }) => state.cartReducer
     );
-
-    const incrementHandler = () => { };
-
-    const decrementHandler = () => { };
-
-    const removeHandler = () => { };
 
     useEffect(() => {
         dispatch(calculatePrice());
@@ -48,9 +40,6 @@ const cart = ({ data }: { data: string }) => {
                         cartItems.length > 0 ? (
                             cartItems.map((i, idx) => (
                                 <CartItemCard
-                                    // incrementHandler={incrementHandler}
-                                    // decrementHandler={decrementHandler}
-                                    // removeHandler={removeHandler}
                                     key={idx}
                                     cartItem={i}
                                 />
@@ -62,36 +51,7 @@ const cart = ({ data }: { data: string }) => {
                         )}
                 </section>
             </main>
-            {/* <aside className='p-4 text-sm w-full flex flex-col justify-start gap-2 bottom-0 bg-orange-200 sm:px-8 sm:text-base sm:flex-row sm:justify-start sm:h-auto sm:bottom-8 md:flex md:w-[20%] md:h-fit md:my-20 md:py-10  md:flex-col md:gap-8 md:justify-center md:rounded-medium shadow-medium' >
-                <div className='flex flex-col justify-start gap-2 sm:w-1/2 md:w-full md:font-medium md:tracking-wider   '>
-                    <p>Subtotal:- ₹{subTotal}</p>
-                    <p>Shipping Charges:- ₹{shippingCharges}</p>
-                    <p>tax:- ₹{tax}</p>
-                    <p>Discount:<em> - ₹{discount}</em></p>
-                    <p><b>Total:- ₹{total}</b></p>
-                </div>
-                <div className='flex flex-col justify-start gap-2 sm:justify-center sm:items-center sm:w-1/2 md:w-full md:items-start'>
-                    <input className='p-2 w-[60%] border border-spacing-1 rounded-lg sm:w-56'
-                        type="text"
-                        placeholder="Coupon Code"
-                        value={couponCode}
-                        onChange={(e) => setCouponCode(e.target.value)}
-                    />
-                    {couponCode && (isValidCouponCode ? (
-                        <span className="green">
-                            ₹{discount} off using the <code>{couponCode}</code>
-                        </span>
-                    ) : (
-                        <span className="red">Invalid Coupon <VscError /></span>
-                    ))}
-                    {cartItems.length > 0 &&
-                        <Link
-                            className='bg-zinc-700 text-white p-3 w-[60%] flex justify-center items-center uppercase space-x-2 font-semibold hover:bg-teal-600 rounded-lg sm:w-56'
-                            href='/shipping'
-                        >Checkout</Link>
-                    }
-                </div>
-            </aside> */}
+
             <div className="bg-orange-100 p-6 h-fit my-20 rounded-lg shadow-md w-full max-w-md">
                 <div className="flex justify-between items-center mb-4">
                     <h2 className="text-xl font-bold">Summary</h2>
@@ -115,21 +75,12 @@ const cart = ({ data }: { data: string }) => {
                     </div>
                 </div>
 
-                <div className="flex mt-4 border-1 border-yellow-950 rounded-md p-2">
-                    <input
-                        type="text"
-                        placeholder="Voucher"
-                        className="w-full px-2 border-none bg-transparent focus:ring-0"
-                    />
-                    <button className="text-indigo-800 px-2 font-semibold">Apply</button>
-                </div>
-
                 <div className="flex justify-between font-bold text-lg mt-4">
                     <span>Total :</span> <span>₹{total}</span>
                 </div>
 
                 <button className="w-full bg-indigo-950 font-semibold text-white py-2 rounded-lg mt-4 hover:bg-zinc-700">
-                    Proceed to check out
+                    <Link href={'/shipping'}>Proceed to check out</Link>
                 </button>
             </div>
         </div>
---
+++ client/src/pages/shipping.tsx
@@ -0,0 +1,257 @@
+import Axios from '@/config/axios';
+import { useAuth } from '@/context/AuthContext';
+import { addToCart, calculatePrice } from '@/redux/reducer/cartReducer';
+import { CartReducerInitialState } from '@/types/reducer-types';
+import { Address, CartItem, User } from '@/types/types';
+import { decryptedData, encryptedData } from '@/utilities/features';
+import { GetServerSideProps } from 'next';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react'
+import { useForm } from 'react-hook-form';
+import { useDispatch, useSelector } from 'react-redux';
+
+const initialAddress = {
+  address: "",
+  city: "",
+  state: "",
+  country: "",
+  pincode: null!,
+  addType: "",
+};
+
+const Shipping = ({ data }: { data: string }) => {
+
+  const { user } = useAuth();
+  const router = useRouter();
+  const dispatch = useDispatch();
+  const [addressInfo, setAddressInfo] = useState<Address[] | []>([]);
+  const [selectedAddressIndex, setSelectedAddressIndex] = useState<number>(0);
+
+  const { register, handleSubmit, formState: { errors }, setValue, reset } = useForm<Address>({
+    defaultValues: initialAddress
+  });
+
+  const { cartItems, subTotal, tax, total, shippingCharges, discount } = useSelector(
+    (state: { cartReducer: CartReducerInitialState }) => state.cartReducer
+  );
+
+  useEffect(() => {
+    if (data) {
+      const items: CartItem[] | [] = decryptedData(data);
+      // setCartItems(cartItems);
+      items.map(item => dispatch(addToCart(item)));
+    }
+  }, [data])
+
+  useEffect(() => {
+    if (user) {
+      setAddressInfo(user.addressInfo);
+    }
+  }, [user]);
+
+  useEffect(() => {
+    if (addressInfo && addressInfo.length > 0) {
+      setSelectedAddressIndex(0);
+    }
+  }, [addressInfo, reset, setValue]);
+
+  useEffect(() => {
+    dispatch(calculatePrice());
+  }, [cartItems])
+
+  const handleSelect = (index: number) => {
+    reset(initialAddress);
+    setSelectedAddressIndex(index);
+  };
+
+  const handleNewAddress = () => {
+    setSelectedAddressIndex(-1);
+    reset(initialAddress);
+  }
+
+  const onSubmit = async () => { }
+
+  return (
+    <div className="w-full min-h-screen flex flex-col lg:flex-row justify-center items-start gap-10 p-6">
+      <div className='w-full max-w-lg p-6 rounded-lg m-2 bg-orange-100 border border-yellow-900 flex flex-col justify-center gap-4 shadow-xl sm:max-w-[80%] lg:max-w-[35%]'>
+        <h5 className="text-lg font-semibold mb-4">Select Shipping Address</h5>
+        {addressInfo &&
+          addressInfo.map((address, index) => (
+            <div key={index} className="flex items-start gap-4">
+              <input
+                type="radio"
+                name="select"
+                checked={selectedAddressIndex === index}
+                onChange={() => handleSelect(index)}
+              />
+              <div>
+                <p className="font-medium">{`${address.addType} Address:`}</p>
+                <p className="text-gray-600">{`${address.address}, ${address.city}, ${address.state}, ${address.country}.`}</p>
+                <p className="text-gray-600"><span className="font-semibold">Pincode:</span> {address.pincode}</p>
+              </div>
+            </div>
+          ))}
+        {/* <button type="button" onClick={handleNewAddress}
+          className="w-full mt-3 py-2 px-4 text-white bg-blue-600 hover:bg-blue-700 rounded-lg transition"
+        >
+          Add New Address
+        </button> */}
+
+        <hr className="border-gray-700" />
+
+
+        <form
+          className="mt-6 space-y-4"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <h1 className="text-lg font-semibold">New Address</h1>
+          <div className="box grid grid-cols-1 gap-4 w-full mt-10 py-3 sm:justify-between sm:px-8 lg:grid-cols-2 lg:gap-8">
+            <div className="inputStyle w-full sm:w-[95%]">
+              <label>Landmark</label>
+              <input
+                placeholder="Landmark"
+                {...register("address", { required: "Landmark is required" })}
+              />
+              {errors.address && <small className="error">{errors.address.message}</small>}
+            </div>
+            <div className="inputStyle w-full sm:w-[95%]">
+              <label>City</label>
+              <input
+                placeholder="City"
+                {...register("city", { required: "City is required" })}
+              />
+              {errors.city && <small className="error">{errors.city.message}</small>}
+            </div>
+            <div className="inputStyle w-full sm:w-[95%]">
+              <label>State</label>
+              <input
+                placeholder="State"
+                {...register("state", { required: "State is required" })}
+              />
+              {errors.state && <small className="error">{errors.state.message}</small>}
+            </div>
+            <div className="inputStyle w-full sm:w-[95%]">
+              <label>Country</label>
+              <select {...register("country", { required: "Country is required" })}>
+                <option value="">Choose Country</option>
+                <option value="India">India</option>
+              </select>
+              {errors.country && <small className="error">{errors.country.message}</small>}
+            </div>
+            <div className="inputStyle w-full sm:w-[95%]">
+              <label>Pincode</label>
+              <input
+                type="number"
+                placeholder="Pincode"
+                {...register("pincode", {
+                  required: "Pincode is required",
+                  minLength: { value: 6, message: "Pincode must be 6 digits" },
+                  maxLength: { value: 6, message: "Pincode must be 6 digits" },
+                })}
+              />
+              {errors.pincode && <small className="error">{errors.pincode.message}</small>}
+            </div>
+          </div>
+          <div className="w-full px-3 flex flex-row gap-2 text-sm lg:gap-4">
+            <button type='submit' className="w-28 bg-indigo-950 hover:bg-zinc-800 text-center text-white  rounded-2xl font-semibold py-2">Save</button>
+            <button type='button' className="w-28 bg-gray-500 hover:bg-gray-700 text-center rounded-2xl text-white font-semibold py-2">Cancel</button>
+          </div>
+        </form>
+      </div>
+
+      <div className="bg-orange-100 p-6 h-fit m-2 rounded-lg shadow-md w-full max-w-md">
+        <div className="flex justify-between items-center mb-4 font-bold text-xl">
+          <h2>Summary</h2>
+          <Link href={'/cart'} className="text-blue-500 text-medium" >Edit Cart</Link>
+        </div>
+
+        <section className='my-8 space-y-4'>
+          {//isLoading ? <Skeleton width="80vw" /> :
+            cartItems.length > 0 ? (
+              cartItems.map((item, index) => (
+                <div key={index} className="flex items-center justify-between">
+                  <Image
+                    src={`${process.env.NEXT_PUBLIC_SERVER}/${item.photo}`}
+                    alt={item.name}
+                    width={50}
+                    height={50}
+                    className="rounded-lg"
+                  />
+                  <div className="flex-1 ml-3">
+                    <p className="text-sm truncate">{item.name}</p>
+                    <span className="text-gray-700 text-xs">x{item.quantity}</span>
+                  </div>
+                  <p className="font-semibold">₹{item.price}</p>
+                </div>
+              ))
+            ) : (
+              <>
+                <h1 className='heading'>No Items Added</h1>
+              </>
+            )}
+        </section>
+
+        <hr className="border-gray-700 my-3" />
+
+        <div className="-tracking-tighter my-8">
+          <div className="flex justify-between py-1">
+            <span>Items subtotal :</span> <span className="font-semibold">₹{subTotal}</span>
+          </div>
+          <div className="flex justify-between py-1">
+            <span>Discount :</span> <span className="font-semibold text-green-600">-₹{discount}</span>
+          </div>
+          <div className="flex justify-between py-1">
+            <span>Tax :</span> <span className="font-semibold">₹{tax}</span>
+          </div>
+          <div className="flex justify-between py-1">
+            <span>Shipping Cost :</span> <span className="font-semibold">₹{shippingCharges}</span>
+          </div>
+          <div className="flex justify-between py-1">
+            <span>Subtotal :</span> <span className="font-semibold">₹{subTotal}</span>
+          </div>
+        </div>
+
+        <hr className="border-gray-700 my-3" />
+
+        <div className="flex my-8 border-1 border-yellow-950 rounded-md p-2">
+          <input
+            type="text"
+            placeholder="Voucher"
+            className="w-full px-2 border-none bg-transparent focus:ring-0 outline-none"
+          />
+          <button className="text-indigo-800 px-2 font-semibold">Apply</button>
+        </div>
+
+        <div className="flex justify-between font-bold text-lg my-4 ">
+          <span>Total :</span> <span>₹{total}</span>
+        </div>
+
+        <button className="w-full bg-indigo-950 font-semibold text-white py-2 rounded-lg mt-4 hover:bg-zinc-700">
+          Proceed to check out
+        </button>
+      </div >
+    </div >
+  )
+}
+
+export default Shipping;
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+
+  const { token } = req.cookies;
+  let cartItems = null;
+
+  try {
+    const { data } = await Axios.get(`/cartItems/all?token=${token}`);
+
+    if (data)
+      cartItems = encryptedData(data.cartItems);
+  } catch (error) {
+    console.log("Error - ", error);
+  }
+  return {
+    props: { data: cartItems }
+  }
+}
